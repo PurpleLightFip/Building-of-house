11a237c5371e19bc1df6a91e9ae72ba9beef2790
PurpleLightFip <fip.purple@gmail.com>
2015-09-25 14:04:18 +0300

    Стена оформлена

--- HouseBuilderModel/Wall.cs
+++ HouseBuilderModel/Wall.cs
@@ -6,33 +6,81 @@ using System.Threading.Tasks;
 
 namespace HouseBuilder
 {
-    class wall
+    class Wall
     {
         //Любую прямую стену можно задать с помощью двух точек координат  
-        public double dbl_x1_coordinate;    //Первая точка
-        public double dbl_y1_coordinate;
-        public double dbl_x2_coordinate;    //Вторая точка
-        public double dbl_y2_coordinate;
+        private double xLeftTop;    //Первая точка
+        private double yLeftTop;
+        private double xRightBottom;    //Вторая точка
+        private double yRightBottom;
 
-        public wall()
+        public Wall()
         {
-            dbl_x1_coordinate = 0;
-            dbl_y1_coordinate = 0;
-            dbl_x2_coordinate = 0;
-            dbl_y1_coordinate = 0;
+            xLeftTop = 0;
+            yLeftTop = 0;
+            xRightBottom = 0;
+            yLeftTop = 0;
         }
 
-        public wall(
-            double dbl_x1_coordinate,
-            double dbl_y1_coordinate,
-            double dbl_x2_coordinate,
-            double dbl_y2_coordinate
+        public Wall(
+            double xLeftTop,
+            double yLeftTop,
+            double xRightBottom,
+            double yRightBottom
             )
         {
-            this.dbl_x1_coordinate = dbl_x1_coordinate;
-            this.dbl_y1_coordinate = dbl_y1_coordinate;
-            this.dbl_x2_coordinate = dbl_x2_coordinate;
-            this.dbl_y2_coordinate = dbl_y2_coordinate;
+            this.xLeftTop = xLeftTop;
+            this.yLeftTop = yLeftTop;
+            this.xRightBottom = xRightBottom;
+            this.yRightBottom = yRightBottom;
         }
+		
+		private double XLeftTop
+		{
+			get
+			{
+				return this.xLeftTop;
+			}
+			set
+			{
+				this.xLeftTop = value < 0 ? 0 : value;
+			}
+		}
+		
+        private double YLeftTop
+		{
+			get
+			{
+				return this.yLeftTop;
+			}
+			set
+			{
+				this.yLeftTop = value < 0 ? 0 : value;
+			}
+		}
+		
+        private double XRightBottom
+		{
+			get
+			{
+				return this.xRightBottom;
+			}
+			set
+			{
+				this.xRightBottom = value < 0 ? 0 : value;
+			}
+		}
+		
+        private double YRightBottom
+		{
+			get
+			{
+				return this.yRightBottom;
+			}
+			set
+			{
+				this.yRightBottom = value < 0 ? 0 : value;
+			}
+		}
     }
 }
